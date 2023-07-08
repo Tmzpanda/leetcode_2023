57095a6641e7518d44147164930993aa3e34ba56
Tim Zhang <tmzpanda@gmail.com>
2023-07-08 15:02:39 -0400

    Update 15. 3Sum.py

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -4,14 +4,14 @@ def threeSum(nums: List[int]) -> List[List[int]]:
     n = len(nums)
     res = []
     for i in range(n):
-        if i > 0 and nums[i] == nums[i-1]:    
+        if i > 0 and nums[i] == nums[i-1]:    # deduplicate
             continue
 
         l, r = i + 1, n - 1
         while l < r:
             s = nums[i] + nums[l] + nums[r]
             if s == 0:
-                if l > i+1 and nums[l] == nums[l-1]:  
+                if l > i + 1 and nums[l] == nums[l-1]:    # deduplicate   
                     l += 1
                     continue
                 res.append([nums[i], nums[l], nums[r]]) 
