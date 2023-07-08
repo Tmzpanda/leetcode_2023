32af88be84f864298629269928c633e2034cbb10
Tim Zhang <tmzpanda@gmail.com>
2023-07-08 13:48:19 -0400

    Create 1. Two Sum.py

---
+++ 1. Two Sum.py
@@ -0,0 +1,9 @@
+def twoSum(nums: List[int], target: int) -> List[int]:
+    num_dict = {}
+    for i, num in enumerate(nums):
+        if target - num in num_dict:
+            return [num_dict[target - num], i]
+        num_dict[num] = i
+
+    return []
+
