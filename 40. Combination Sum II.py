97a42b649011f6c3a51ff13d1e9c55b3c64e3c29
Tim Zhang <tmzpanda@gmail.com>
2023-07-11 11:10:12 -0400

    Create 40. Combination Sum II.py

---
+++ 40. Combination Sum II.py
@@ -0,0 +1,21 @@
+def combinationSum2(nums: List[int], target: int) -> List[List[int]]:
+    nums = sorted(nums)
+    n = len(nums)
+    res = []
+    def dfs(start, target, combination):
+        if target == 0:
+            res.append(list(combination)) 
+            return
+        if target < 0:
+            return 
+
+        for i in range(start, n):
+            if i > 0 and nums[i] == nums[i - 1] and i != start:    # deduplicate
+                continue
+            combination.append(nums[i])
+            dfs(i + 1, target - nums[i], combination)    # used once
+            combination.pop()
+        
+    dfs(0, target, [])
+    return res
+        
