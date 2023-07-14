06b9562372f40c328f782162347777b1527b491f
Tim Zhang <tmzpanda@gmail.com>
2023-07-14 13:07:56 -0700

    Update 416. Partition Equal Subset Sum.py

--- 416. Partition Equal Subset Sum.py
+++ 416. Partition Equal Subset Sum.py
@@ -11,12 +11,15 @@ def canPartition(nums: List[int]) -> bool:
     def dfs(i, s):
         if s == 0:
             return True
-        if i == 0 or s < 0:
+        if i == -1:
             return False
         if (i, s) in memo:
             return memo[(i, s)]
         
-        res = dfs(i - 1, s - nums[i]) or dfs(i - 1, s)
+        if nums[i] <= s:
+            res = dfs(i - 1, s - nums[i]) or dfs(i - 1, s)
+        else:
+            res = dfs(i - 1, s)
 
         memo[(i, s)] = res
         return res
@@ -25,6 +28,17 @@ def canPartition(nums: List[int]) -> bool:
     
   
 # dp
+"""
+nums = [1, 4, 5]
+
+    0  1  2  3  4  5  subset_sum
+nums
+0   1  
+1   1   
+4   1o             o
+5   1              x = dp[i][s] denotes #of ways to make change for amount s with coins[:i]
+
+"""
 def canPartition(nums: List[int]) -> bool:
     n = len(nums)
     total_sum = sum(nums)
@@ -33,7 +47,8 @@ def canPartition(nums: List[int]) -> bool:
     subset_sum = total_sum // 2
 
     dp = [[False] * (subset_sum + 1) for _ in range(n + 1)]
-    dp[0][0] = True
+    for i in range(n + 1):
+        dp[i][0] = 1
 
     for i in range(1, n + 1):
         for s in range(subset_sum + 1):
@@ -57,7 +72,7 @@ def canPartition(nums: List[int]) -> bool:
     dp[0] = True
 
     for num in nums:
-        for s in range(subset_sum, num - 1, -1):  # iterate backwards
+        for s in range(subset_sum, num - 1, -1):    # iterate backwards
             dp[s] = dp[s] or dp[s - num]
 
     return dp[subset_sum]
