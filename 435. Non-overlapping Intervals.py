a731fb99d4a56e51c3b66471f4a6580e38cced40
Tim Zhang <tmzpanda@gmail.com>
2023-07-18 17:54:11 -0400

    Create 435. Non-overlapping Intervals.py

---
+++ 435. Non-overlapping Intervals.py
@@ -0,0 +1,25 @@
+"""
+x-----x 
+      x---x
+
+x-----x
+    x---x
+
+x-----x
+  x--x
+
+"""
+
+def eraseOverlapIntervals(intervals: List[List[int]]) -> int:
+    intervals = sorted(intervals)
+
+    res = 0
+    prevEnd = intervals[0][1]
+    for start, end in intervals[1:]:
+        if start >= prevEnd:
+            prevEnd = end
+        else:
+            res += 1
+            prevEnd = min(end, prevEnd)
+
+    return res
