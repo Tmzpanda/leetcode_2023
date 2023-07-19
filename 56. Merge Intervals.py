8cc86c0c6117ab2a6aa2a357591a1722a5e8d067
Tim Zhang <tmzpanda@gmail.com>
2023-07-19 08:03:02 -0700

    Create 56. Merge Intervals.py

---
+++ 56. Merge Intervals.py
@@ -0,0 +1,12 @@
+def merge(intervals: List[List[int]]) -> List[List[int]]:  
+    intervals = sorted(intervals)
+
+    res = [intervals[0]]
+    for start, end in intervals[1:]:
+        prevEnd = res[-1][1]
+        if start > prevEnd:
+            res.append([start, end])
+        else:
+            res[-1][1] = max(prevEnd, end)
+        
+    return res
