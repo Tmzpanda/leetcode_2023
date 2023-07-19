9aaec34535a8d7cee861f0b751573f1ba0f3a068
Tim Zhang <tmzpanda@gmail.com>
2023-07-19 08:09:20 -0700

    Create 252. Meeting Rooms.py

---
+++ 252. Meeting Rooms.py
@@ -0,0 +1,11 @@
+def canAttendMeetings(intervals: List[List[int]]) -> bool:
+    intervals = sorted(intervals)
+
+    prevEnd = intervals[0][1]
+    for start, end in intervals[1:]:
+        if start >= prevEnd:
+            prevEnd = end
+        else:
+            return False
+
+    return True
