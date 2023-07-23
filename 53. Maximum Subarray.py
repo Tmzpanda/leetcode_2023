cc9a2138a6689eb70a23458c8a7e329602a39c14
Tim Zhang <tmzpanda@gmail.com>
2023-07-23 14:30:09 -0700

    Update 53. Maximum Subarray.py

--- 53. Maximum Subarray.py
+++ 53. Maximum Subarray.py
@@ -2,8 +2,9 @@
 def maxSubArray(nums):
     res = -float('inf')
     psum = 0
-
     for num in nums:
+        if psum < 0:
+            psum = 0
         psum += num
         res = max(res, psum)
 
