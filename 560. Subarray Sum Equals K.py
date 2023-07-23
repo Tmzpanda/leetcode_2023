e1549b1e3bd1992a27f15e5e337a62e877a7fdf8
Tim Zhang <tmzpanda@gmail.com>
2023-07-23 14:32:17 -0700

    Create 560. Subarray Sum Equals K.py

---
+++ 560. Subarray Sum Equals K.py
@@ -0,0 +1,14 @@
+def subarraySum(nums: List[int], k: int) -> int:
+    psum = 0
+    psum_freq_dict = defaultdict(int)
+    psum_freq_dict[0] = 1
+    res = 0
+    
+    for num in nums: 
+        psum += num
+        if psum - k in psum_freq_dict:
+            res += psum_freq_dict[psum - k]
+        psum_freq_dict[psum] += 1
+    
+    return res
+            
