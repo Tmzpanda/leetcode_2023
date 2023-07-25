b6dbeddfbd8ae57a26bbf53e5fc492409b622497
Tim Zhang <tmzpanda@gmail.com>
2023-07-25 09:52:27 -0700

    Create 108. Convert Sorted Array to Binary Search Tree.py

---
+++ 108. Convert Sorted Array to Binary Search Tree.py
@@ -0,0 +1,16 @@
+def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+    def dfs(l, r):  
+        # base
+        if l > r:
+            return None
+        # operate
+        mid = (l + r) // 2
+        root = TreeNode(nums[mid])
+        # d&q
+        root.left = dfs(l, mid - 1)
+        root.right = dfs(mid + 1, r)
+        
+        return root
+
+    return dfs(0, len(nums) - 1)
+  
