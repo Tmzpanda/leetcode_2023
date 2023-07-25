3da372932068e4fb507134cac8d3c9334a4c3c02
Tim Zhang <tmzpanda@gmail.com>
2023-07-25 09:49:35 -0700

    Update 912. Sort an Array.py

--- 912. Sort an Array.py
+++ 912. Sort an Array.py
@@ -26,28 +26,27 @@ def mergeSort(nums: List[int]) -> List[int]:
 
 
 # O(nlogn) O(1)
-def quickSort(nums, start, end):
-    # base
-    if start >= end:      
-        return
+def sortArray(nums: List[int]) -> List[int]:
+   def quickSort(start, end):
+        # base
+        if start >= end:      
+            return
+        # partition
+        pivot = nums[random.randint(start, end)]
+        l, r = start, end
+        while l <= r:
+            while l <= r and nums[l] < pivot:
+                l += 1
+            while l <= r and nums[r] > pivot:
+                r -= 1
+            if l <= r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+        # d&q
+        quickSort(start, r)
+        quickSort(l, end)
 
-    # partition
-    l, r = start, end
-    pivot = random.randint(l, r)
-    while l <= r:
-        while l <= r and nums[l] < nums[pivot]:
-            l += 1
-        while l <= r and nums[r] > nums[pivot]:
-            r -= 1
-        if l <= r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l += 1
-            r -= 1
-    # d&q
-    quickSort(nums, start, r)
-    quickSort(nums, l, end)
-    
-    
-    
-    
-    
+    quickSort(0, len(nums) - 1)
+    return nums
+
