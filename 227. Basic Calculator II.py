f8f65f245f8d435bbe1a2cb4d2ad62fc5d075854
Tim Zhang <tmzpanda@gmail.com>
2023-08-02 16:44:01 -0700

    Create 227. Basic Calculator II.py

---
+++ 227. Basic Calculator II.py
@@ -0,0 +1,18 @@
+def calculate(s: str) -> int:
+    stack = []
+    pre_op = '+'
+    num = 0
+    for c in s+'+':
+        if c.isdigit():
+            num = num*10 + int(c)
+            
+        if c in ('+', '-', '*', '/'):
+            if pre_op == '+': stack.append(num)
+            elif pre_op == '-': stack.append(-num)
+            elif pre_op == '*': stack.append(stack.pop()*num)
+            elif pre_op == '/': stack.append(math.trunc(stack.pop()/num))   # -3/2
+
+            pre_op = c
+            num = 0
+            
+    return sum(stack)
