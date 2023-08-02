9c74b1037020d08c382dab1e0010b609ef9fc655
Tim Zhang <tmzpanda@gmail.com>
2023-08-02 15:58:02 -0700

    Update 291. Word Pattern II.py

--- 291. Word Pattern II.py
+++ 291. Word Pattern II.py
@@ -1,32 +1,25 @@
 def wordPatternMatch(pattern: str, s: str) -> bool:
     def dfs(pattern, s):
-        if not pattern:
+        if not pattern: 
             return not s
 
         c = pattern[0]
-        if c in charToWord:
+        if c not in charToWord:
+            for i in range(len(s)):
+                word = s[:i+1]
+                if word in wordSet: continue
+                # backtrack
+                charToWord[c] = word
+                wordSet.add(word)
+                if dfs(pattern[1:], s[i+1:]): return True
+                del charToWord[c]
+                wordSet.remove(word)
+        else:
             word = charToWord[c]
-            if not s.startswith(word):
-                return False
+            if not s.startswith(word): return False
             return dfs(pattern[1:], s[len(word):])
-        
-        for i in range(len(s)):
-            word = s[:i+1]
-            if word in wordToChar:
-                continue
-
-            charToWord[c] = word
-            wordToChar[word] = c
-            
-            if dfs(pattern[1:], s[i+1:]):
-                return True
-            
-            del charToWord[c]
-            del wordToChar[word]
 
     charToWord = {}
-    wordToChar = {}
+    wordSet = set()
     return dfs(pattern, s)
 
-    
-    
