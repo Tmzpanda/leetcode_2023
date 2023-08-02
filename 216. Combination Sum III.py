64373cd4a40a4826ec1e96aefd2351ca0d51fd4c
Tim Zhang <tmzpanda@gmail.com>
2023-08-02 14:26:52 -0700

    Update 216. Combination Sum III.py

--- 216. Combination Sum III.py
+++ 216. Combination Sum III.py
@@ -16,21 +16,3 @@ def combinationSum3(k: int, n: int) -> List[List[int]]:
     
     dfs(1, n, [])
     return res
-
-# another implementation
-def combinationSum3(k: int, n: int) -> List[List[int]]:
-    res = []
-    def dfs(start, total, path):
-        if total == n and len(path) == k:
-            res.append(list(path))
-            return
-        if total > n or len(path) > k:
-            return
-        
-        for i in range(start, 10):
-            path.append(i)
-            dfs(i + 1, total + i, path)
-            path.pop()
-    
-    dfs(1, 0, [])
-    return res
