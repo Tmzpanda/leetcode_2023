c41a52cd5a40528b24a365321a033d9c93139c05
Tim Zhang <tmzpanda@gmail.com>
2023-08-02 19:35:51 -0700

    Create 402. Remove K Digits.py

---
+++ 402. Remove K Digits.py
@@ -0,0 +1,11 @@
+def removeKdigits(num: str, k: int) -> str:
+    stack = []
+    for digit in num:
+        while stack and digit < stack[-1] and k:    
+            stack.pop()
+            k -= 1
+        stack.append(digit)
+    
+    res= stack[:-k] if k else stack
+    return "".join(res).lstrip('0') or "0"
+        
