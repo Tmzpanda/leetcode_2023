2a871e3ecb115b59b8ebb41e38f687cabe75b262
Tim Zhang <tmzpanda@gmail.com>
2023-08-04 13:50:27 -0700

    Update 10. Regular Expression Matching.py

--- 10. Regular Expression Matching.py
+++ 10. Regular Expression Matching.py
@@ -1,36 +1,41 @@
-"""
-"aab" "c*a*b"
-"a"   "a*b*"
-
-                "c*"
-              + /  \ -   
-              "c"  ""    
-       (i+1, j)      (i, j+2)     
-          
-
-"""
 def isMatch(s: str, p: str) -> bool:
-    i, j = len(s) - 1, len(p) - 1
+    memo = {}
     def dfs(i, j):
+        # memo
+        if (i, j) in memo:
+            return memo[(i, j)]
+
+        # p is used out
         if j == -1 and i == -1: return True
         if j == -1 and i != -1: return False
-
+        # i is used out
         if i == -1 and p[j] != '*': return False
-        if i == -1 and p[j] == '*':    
+        if i == -1 and p[j] == '*':
             k = j
             while k != -1 and p[k] == '*':
                 k -= 2
-            return k == -1
-
-        if p[j] == '.' or s[i] == p[j]:
-            if dfs(i - 1, j - 1): return True
-        if p[j] == '*':
-            # use *
-            if dfs(i, j - 2): return True
-            # not use *  
-            if (p[j - 1] == s[i] or p[j - 1] == '.') and dfs(i - 1, j): return True
+            res = (k == -1)
+        # *
+        elif p[j] == '*':
+            res = (
+                (dfs(i - 1, j) and (p[j - 1] == s[i] or p[j - 1] == '.')) or
+                dfs(i, j - 2)       
+            )
+        # .
+        elif p[j] == '.' or s[i] == p[j]:
+            res = dfs(i - 1, j - 1)
+        else:
+            res = False
         
-        return False
-      
+        memo[(i, j)] = res
+        return res
+    
+    i, j = len(s) - 1, len(p) - 1
     return dfs(i, j)
 
+
+            
+            
+
+
+        
