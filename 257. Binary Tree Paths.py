dba84ed5d8573260fc8da4bea74fe31614c8c0a2
Tim Zhang <tmzpanda@gmail.com>
2023-08-06 18:28:41 -0700

    Create 257. Binary Tree Paths.py

---
+++ 257. Binary Tree Paths.py
@@ -0,0 +1,16 @@
+def binaryTreePaths(root: TreeNode) -> List[str]:
+    def dfs(node, path):
+        if not node.left and not node.right:
+            res.append(list(path))
+            return
+
+        for node in (node.left, node.right):
+            if node:
+                path.append(str(node.val))
+                dfs(node, path)
+                path.pop()       # backtrack
+
+    res = []
+    dfs(root, [str(root.val)])
+
+    return ['->'.join(path) for path in res]
