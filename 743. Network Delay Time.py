cd0b061f7a4143280b44e1ce218f3ab1b8c88581
Tim Zhang <tmzpanda@gmail.com>
2023-08-06 18:33:59 -0700

    Create 743. Network Delay Time.py

---
+++ 743. Network Delay Time.py
@@ -0,0 +1,19 @@
+def networkDelayTime(times: List[List[int]], n: int, k: int) -> int:
+    # build graph
+    graph = defaultdict(list)
+    for f, t, w in times:
+        graph[f].append((t, w))
+
+    # dijkstra
+    min_heap = [(0, k)] 
+    visited = set()
+    while min_heap:
+        weight, node = heappop(min_heap)
+        visited.add(node)
+        if len(visited) == n:
+            return weight
+
+        for next_node, next_weight in graph[node]:      # directed graph
+            heappush(min_heap, (weight + next_weight, next_node)) 
+
+    return -1
