9fd3853d8f64afc1b48a9f93372132df04f8109d
Tim Zhang <tmzpanda@gmail.com>
2023-08-12 10:55:58 -0700

    Update 862. Shortest Subarray with Sum at Least K.py

--- 862. Shortest Subarray with Sum at Least K.py
+++ 862. Shortest Subarray with Sum at Least K.py
@@ -3,15 +3,15 @@ def shortestSubarray(self, nums: List[int], k: int) -> int:
     psum = 0
     res = float('inf')
 
-    monoq = deque([(-1, 0)])    # (i, psum)
+    psumq = deque([(-1, 0)])    # (i, psum)
     for i in range(n):
         psum += nums[i]
-        while monoq and psum - monoq[0][1] >= k:
-            res = min(res, i-monoq[0][0])
-            monoq.popleft()
-        while monoq and psum <= monoq[-1][1]:    # monoq
-            monoq.pop()
-        monoq.append((i, psum))
+        while psumq and psum - psumq[0][1] >= k:
+            res = min(res, i-psumq[0][0])
+            psumq.popleft()
+        while psumq and psum <= psumq[-1][1]:    # mono-increasing
+            psumq.pop()
+        psumq.append((i, psum))
 
     return res if res != float('inf') else -1
-    
+
