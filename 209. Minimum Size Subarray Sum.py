105ffb2b250b927a2b1ecfb549803e950121ba5c
Tim Zhang <tmzpanda@gmail.com>
2023-08-12 10:23:07 -0700

    Update 209. Minimum Size Subarray Sum.py

--- 209. Minimum Size Subarray Sum.py
+++ 209. Minimum Size Subarray Sum.py
@@ -12,3 +12,19 @@ def minSubArrayLen(target: int, nums: List[int]) -> int:
             start += 1
 
     return res if res != float('inf') else 0
+
+
+def minSubArrayLen(target: int, nums: List[int]) -> int:
+    n = len(nums)
+    psum = 0
+    res = float('inf')
+
+    psumq = deque([(-1, 0)])
+    for i in range(n):
+        psum += nums[i]
+        while psumq and psum - psumq[0][1] >= target:
+            res = min(res, i - psumq[0][0])
+            psumq.popleft()
+        psumq.append((i, psum))
+
+    return res if res != float('inf') else 0
