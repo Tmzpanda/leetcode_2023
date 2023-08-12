409d79d5510eeb97a7dbddca5db07223950a45c4
Tim Zhang <tmzpanda@gmail.com>
2023-08-12 11:17:05 -0700

    Update 402. Remove K Digits.py

--- 402. Remove K Digits.py
+++ 402. Remove K Digits.py
@@ -1,11 +1,11 @@
 def removeKdigits(num: str, k: int) -> str:
     stack = []
     for digit in num:
-        while stack and digit < stack[-1] and k:    
+        while stack and digit < stack[-1] and k:    # mono-increasing
             stack.pop()
             k -= 1
         stack.append(digit)
     
-    res= stack[:-k] if k else stack
+    res = stack[:-k] if k else stack
     return "".join(res).lstrip('0') or "0"
         
