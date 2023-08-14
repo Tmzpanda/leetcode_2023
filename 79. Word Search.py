0688e3fdb428aef82a37db698adcf83542287bc5
Tim Zhang <tmzpanda@gmail.com>
2023-08-14 08:33:28 -0700

    Update 79. Word Search.py

--- 79. Word Search.py
+++ 79. Word Search.py
@@ -1,20 +1,16 @@
 def exist(board: List[List[str]], word: str) -> bool:
-    m, n = len(board), len(board[0])
-    directions = [(-1, 0), (0, 1), (0, -1), (1, 0)]
-    
     def dfs(i, j, index):
         if index == len(word):
             return True
-
-        for delta_i, delta_j in directions:
-            x, y = i + delta_i, j + delta_j
-            if 0 <= x < len(board) and 0 <= y < len(board[0]) and (x, y) not in visited and board[x][y] == word[index]:
-                visited.add((x, y))
+        for x, y in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
+            if 0 <= x < m and 0 <= y < n and (x, y) not in visited and board[x][y] == word[index]:
+                visited.add((x, y)) 
                 if dfs(x, y, index + 1):
                     return True
-                visited.remove((x, y))         # backtrack
+                visited.remove((x, y))  # backtrack         
         return False
 
+    m, n = len(board), len(board[0])
     visited = set()
     for i in range(m):
         for j in range(n):
@@ -25,3 +21,4 @@ def exist(board: List[List[str]], word: str) -> bool:
                 
     return False
     
+            
