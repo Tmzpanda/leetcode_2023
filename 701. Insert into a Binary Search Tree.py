e956b4ac86b360f0e3f8234d54e219aec9a8ee55
Tim Zhang <tmzpanda@gmail.com>
2023-08-15 10:13:46 -0700

    Update 701. Insert into a Binary Search Tree.py

--- 701. Insert into a Binary Search Tree.py
+++ 701. Insert into a Binary Search Tree.py
@@ -8,3 +8,19 @@ def insertIntoBST(root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
         root.right = insertIntoBST(root.right, val)
     
     return root
+
+
+# another implementation
+def insertIntoBST(root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+    def dfs(root):
+        if not root:
+            return TreeNode(val)
+
+        if root.val > val:
+            root.left = dfs(root.left)
+        else:
+            root.right = dfs(root.right)
+            
+        return root
+        
+    return dfs(root)
