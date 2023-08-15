4e7e1fde8af05fbb6515a50a60ee716f5cb3ad2a
Tim Zhang <tmzpanda@gmail.com>
2023-08-15 10:30:35 -0700

    Create 450. Delete Node in a BST.py

---
+++ 450. Delete Node in a BST.py
@@ -0,0 +1,26 @@
+def deleteNode(root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+    if not root:
+        return
+    
+    if root.val > key:
+        root.left = deleteNode(root.left, key)
+    elif root.val < key:
+        root.right = deleteNode(root.right, key)
+    else:
+        # no child or one child
+        if not root.left:
+            return root.right
+        if not root.right:
+            return root.left
+        # two children
+        else:
+            temp = findSmallest(root.right)
+            root.val = temp.val
+            root.right = deleteNode(root.right, temp.val)
+        
+    return root
+
+def findSmallest(self, root):
+    while root.left:
+        root = root.left
+    return root
