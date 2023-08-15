baf16dbbb7055d9b3d7322c4cd3fbc4d73a9f2c6
Tim Zhang <tmzpanda@gmail.com>
2023-08-15 11:42:45 -0700

    Create 235. Lowest Common Ancestor of a Binary Search Tree.py

---
+++ 235. Lowest Common Ancestor of a Binary Search Tree.py
@@ -0,0 +1,8 @@
+def lowestCommonAncestor(root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    while root:
+        if root.val > p.val and root.val > q.val:
+            root = root.left
+        elif root.val < p.val and root.val < q.val:
+            root = root.right
+        else:
+            return root
