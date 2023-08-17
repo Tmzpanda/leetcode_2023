aea42643b368a10e1c54c1f2c820db0eb0278855
Tim Zhang <tmzpanda@gmail.com>
2023-08-17 15:50:53 -0700

    Update 256. Paint House.py

--- 256. Paint House.py
+++ 256. Paint House.py
@@ -1,21 +1,21 @@
 def minCost(costs: List[List[int]]) -> int:
     n = len(costs)
     memo = {}
-    def dfs(i, prevColor):
+    def dfs(i, prevC):
         # memo
-        if (i, prevColor) in memo:
-            return memo[(i, prevColor)]
+        if (i, prevC) in memo:
+            return memo[(i, prevC)]
         # base
         if i == -1:
             return 0
 
         res = float('inf')
         for c in range(3):
-            if c == prevColor:
+            if c == prevC:
                 continue
             res = min(res, costs[i][c]+dfs(i-1, c))
         
-        memo[(i, prevColor)] = res
-        return memo[(i, prevColor)]
+        memo[(i, prevC)] = res
+        return memo[(i, prevC)]
         
     return dfs(n-1, -1)
