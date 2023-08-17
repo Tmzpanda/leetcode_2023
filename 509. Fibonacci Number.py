ea15f7a66e3b16271be8822e23f96a97fb752a59
Tim Zhang <tmzpanda@gmail.com>
2023-08-17 09:57:44 -0700

    Create 509. Fibonacci Number.py

---
+++ 509. Fibonacci Number.py
@@ -0,0 +1,29 @@
+# dfs
+def fib(n: int) -> int:
+    memo = {}
+    def dfs(n):
+        # memo
+        if n in memo:
+            return memo[n]
+        # base
+        if n == 0 or n == 1:
+            return n
+
+        memo[n] = dfs(n - 1) + dfs(n - 2)
+        return memo[n]
+    
+    return dfs(n - 1) + dfs(n - 2)
+
+
+# dp
+def fib(self, n: int) -> int:
+    if n == 0 or n == 1:
+        return n
+
+    # base
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    for i in range(2, n + 1):
+        dp[i] = dp[i-1] + dp[i-2]
+        
+    return dp[n]
