bd604fd19abd938888c2498b8ab58d6c48dc27d1
Tim Zhang <tmzpanda@gmail.com>
2023-08-21 10:12:55 -0700

    Update 127. Word Ladder.py

--- 127. Word Ladder.py
+++ 127. Word Ladder.py
@@ -1,13 +1,11 @@
 def ladderLength(beginWord: str, endWord: str, wordList: List[str]) -> int:
     wordList.append(beginWord)
-    n, m = len(wordList), len(beginWord)
+    n = len(wordList)
     
-    # build graph
-    graph = defaultdict(list)       
+    pattern_dict = defaultdict(list)       
     for word in wordList:
-        for i in range(m):
-            pattern = word[:i]+'*'+word[i+1:]
-            graph[pattern].append(word)
+        for i in range(len(word)):
+            pattern_dict[word[:i]+'*'+word[i+1:]].append(word)
 
     # bfs
     visited = set([beginWord])
@@ -18,19 +16,13 @@ def ladderLength(beginWord: str, endWord: str, wordList: List[str]) -> int:
             word = q.popleft()
             if word == endWord:
                 return res
-            for i in range(m):
-                pattern = word[:i]+'*'+word[i+1:]
-                for nei in graph[pattern]:
-                    if nei not in visited:
-                        visited.add(nei)
-                        q.append(nei)
+            for nei in [nei for i in range(len(word)) for nei in pattern_dict[word[:i]+'*'+word[i+1:]]]:
+                if nei not in visited:
+                    visited.add(nei)
+                    q.append(nei)
         res += 1
     
     return 0
-
-
-
-
 
 
 
