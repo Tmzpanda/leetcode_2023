675d6ed1a1d305806bb9d71f1f6282a964065096
Tim Zhang <tmzpanda@gmail.com>
2023-08-21 11:16:24 -0700

    Update 242. Valid Anagram.py

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -6,9 +6,9 @@ def isAnagram(s: str, t: str) -> bool:
 # dict
 from collections import defaultdict
 def isAnagram(s: str, t: str) -> bool:
-    char_freq = defaultdict(int)
+    charToFreq = defaultdict(int)
     for char in s:
-        char_freq[char] += 1
+        charToFreq[char] += 1
     for char in t:
-        char_freq[char] -= 1
-    return all(v == 0 for v in char_freq.values())
+        charToFreq[char] -= 1
+    return all(v == 0 for v in charToFreq.values())
