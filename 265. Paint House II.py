aa8036359b90132219ab7243c963d1c2ca0b5d11
Tim Zhang <tmzpanda@gmail.com>
2023-08-22 10:09:46 -0700

    Create 265. Paint House II.py

---
+++ 265. Paint House II.py
@@ -0,0 +1,21 @@
+def minCostII(costs: List[List[int]]) -> int:
+    n = len(costs)
+    memo = {}
+    def dfs(i, prevC):
+        # memo
+        if (i, prevC) in memo:
+            return memo[(i, prevC)]
+        # base
+        if i == -1:
+            return 0
+
+        res = float('inf')
+        for c in range(len(costs[0])):
+            if c == prevC:
+                continue
+            res = min(res, costs[i][c]+dfs(i-1, c))
+        
+        memo[(i, prevC)] = res
+        return memo[(i, prevC)]
+        
+    return dfs(n-1, -1)
