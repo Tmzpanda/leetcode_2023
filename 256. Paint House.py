4d140012affb39dba8fa46f6590a0bf60d9da40d
Tim Zhang <tmzpanda@gmail.com>
2023-08-22 10:45:52 -0700

    Update 256. Paint House.py

--- 256. Paint House.py
+++ 256. Paint House.py
@@ -1,3 +1,4 @@
+# dfs
 def minCost(costs: List[List[int]]) -> int:
     n = len(costs)
     memo = {}
@@ -19,3 +20,48 @@ def minCost(costs: List[List[int]]) -> int:
         return memo[(i, prevC)]
         
     return dfs(n-1, -1)
+
+# dp O(n*3)
+"""
+costs = [[17,2,17],[16,16,5],[14,3,19]]
+
+      c
+      0  1  2 
+house 0  0  0 
+    0 
+    1 o  o
+    2       x = dp[i][c] denotes costs of paint the first i houses, while painting ith house with color c
+
+"""
+def minCost(costs: List[List[int]]) -> int:
+    n = len(costs)
+    dp = [[0] * 3 for _ in range(n)]
+    dp[0][0], dp[0][1], dp[0][2] = costs[0][0], costs[0][1], costs[0][2]
+    for i in range(1, n):
+        dp[i][0], dp[i][1], dp[i][2] = (
+            costs[i][0] + min(dp[i-1][1], dp[i-1][2]), 
+            costs[i][1] + min(dp[i-1][0], dp[i-1][2]), 
+            costs[i][2] + min(dp[i-1][0], dp[i-1][1])
+        )
+
+    return min(dp[n-1])
+
+# space optimization O(2*3)
+def minCost(costs: List[List[int]]) -> int:
+    n = len(costs)
+    prev_dp = costs[0][:]
+    for i in range(1, n):
+        curr_dp = [0] * 3
+        curr_dp[0], curr_dp[1], curr_dp[2] = (
+            costs[i][0] + min(prev_dp[1], prev_dp[2]),
+            costs[i][1] + min(prev_dp[0], prev_dp[2]),
+            costs[i][2] + min(prev_dp[0], prev_dp[1]))
+        prev_dp = curr_dp
+
+    return min(prev_dp)
+
+
+
+
+
+
