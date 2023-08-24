02174ab39fd7f1716fb6adf16fafab3a17a21f97
Tim Zhang <tmzpanda@gmail.com>
2023-08-24 12:18:32 -0700

    Update 300. Longest Increasing Subsequence.py

--- 300. Longest Increasing Subsequence.py
+++ 300. Longest Increasing Subsequence.py
@@ -1,3 +1,24 @@
+# dfs
+def lengthOfLIS(nums: List[int]) -> int:
+     n = len(nums)
+     memo = {}
+     def dfs(i):
+         if i in memo:
+             return memo[i]
+
+         memo[i] = 1
+         for j in range(0, i):
+             if nums[j] < nums[i]:
+                 memo[i] = max(memo[i], dfs(j)+1)
+         return memo[i]
+
+     s = 1
+     for i in range(n):
+         s = max(s, dfs(i))
+         
+     return s
+
+# dp O(n^2)
 """
  0  1  2  3
 [1, 2, 4, 3]
@@ -8,7 +29,6 @@ LIS[2] = max(1, 1+LIS[0], 1+LIS[1]) = 3
 LIS[3] = max(1, 1+LIS[0], 1+LIS[1]) = 3
 
 """
-# O(n^2)
 def lengthOfLIS(nums: List[int]) -> int:
     n = len(nums)        
     dp = [1] * n
