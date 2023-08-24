9b2383971d2e75a7b3a5a8f96b9b4b3858cedbae
Tim Zhang <tmzpanda@gmail.com>
2023-08-24 11:54:49 -0700

    Update 213. House Robber II.py

--- 213. House Robber II.py
+++ 213. House Robber II.py
@@ -6,24 +6,6 @@ def rob(nums: List[int]) -> int:
     def dfs(nums, i, memo):
         if i in memo:
             return memo[i]
-        if i < 0:
-            return 0
-
-        memo[i] = max(nums[i] + dfs(nums, i-2, memo), dfs(nums, i-1, memo))
-        return memo[i]
-
-    return max(
-        dfs(nums[1:], n-2, {}), 
-        dfs(nums[:-1], n-2, {})
-        ) 
-
-def rob(nums: List[int]) -> int:
-    n = len(nums)
-    if n == 1: return nums[0]
-
-    def dfs(nums, i, memo):
-        if i in memo:
-            return memo[i]
         if i >= n-1:
             return 0
 
