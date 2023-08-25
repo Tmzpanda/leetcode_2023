d302aba60e10f047e73888a4f7ec7dd57c4612a1
Tim Zhang <tmzpanda@gmail.com>
2023-08-24 18:06:45 -0700

    Create 429. N-ary Tree Level Order Traversal.py

---
+++ 429. N-ary Tree Level Order Traversal.py
@@ -0,0 +1,17 @@
+def levelOrder(root: 'Node') -> List[List[int]]:
+    if root is None:
+        return []
+        
+    res = []
+    queue = deque([root])
+    while queue:
+        level = []
+        for _ in range(len(queue)):
+            node = queue.popleft()
+            level.append(node.val)
+            for node in node.children:
+                if node:
+                    queue.append(node)
+        res.append(level)
+        
+    return res
