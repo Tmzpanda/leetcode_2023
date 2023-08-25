c0f8777104ff3cc3c4c6a9b7065277fd8872b030
Tim Zhang <tmzpanda@gmail.com>
2023-08-25 09:09:15 -0700

    Update 62. Unique Paths.py

--- 62. Unique Paths.py
+++ 62. Unique Paths.py
@@ -1,6 +1,6 @@
 # dp
 def uniquePaths(m: int, n: int) -> int:
-    dp = [[0]*(n+1) for _ in range(m+1)]
+    dp = [[0]* (n+1) for _ in range(m+1)]
     dp[0][1] = 1
     
     for i in range(1, m + 1):
