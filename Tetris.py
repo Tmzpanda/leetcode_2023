a2aba52648600abc61fef7f09dc914bd0cce7844
Tim Zhang <tmzpanda@gmail.com>
2023-09-07 10:56:51 -0700

    Update Tetris.py

--- Tetris.py
+++ Tetris.py
@@ -1,18 +1,3 @@
-# rigid
-"""
-[['.', 'F', 'F', 'F', '.'],   
- ['.', '.', 'F', '.', '.'],
- ['#', '.', '.', '#', '.'],
- ['.', '.', '.', '.', '.']]
-=> 
-[['.', '.', '.', '.', '.'],
- ['.', 'F', 'F', 'F', '.'],
- ['#', '.', 'F', '#', '.'],
- ['.', '.', '.', '.', '.']]
- 
-"""
-
-
 # soft
 """
 [['.', 'F', 'F', 'F', '.'],   
@@ -42,3 +27,35 @@ def simulate_falling(matrix):
     return matrix
 
 
+# rigid
+"""
+[['.', 'F', 'F', 'F', '.'],   
+ ['.', '.', 'F', '.', '.'],
+ ['#', '.', '.', '#', '.'],
+ ['.', '.', '.', '.', '.']]
+=> 
+[['.', '.', '.', '.', '.'],
+ ['.', 'F', 'F', 'F', '.'],
+ ['#', '.', 'F', '#', '.'],
+ ['.', '.', '.', '.', '.']]
+ 
+"""
+def simulate_falling(matrix):
+    m, n = len(matrix), len(matrix[0])
+ 
+    min_distance = float('inf')   
+    for j in range(n):
+        spot = m - 1
+        for i in range(m - 1, -1 ,-1):
+            if matrix[i][j] == '#':
+                spot = i - 1
+            if matrix[i][j] == 'F':
+                min_distance = min(spot-i, min_distance)
+
+    for i in range(m-1, -1, -1):
+        for j in range(n):
+            if matrix[i][j] == 'F' and min_distance != 0:
+                matrix[i+min_distance][j] = 'F'
+                matrix[i][j] = '.'
+                
+    return matrix
