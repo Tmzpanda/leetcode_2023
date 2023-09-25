8d7b0836604327df4a87bc7b3934de51e5f4c3e7
Tim Zhang <tmzpanda@gmail.com>
2023-09-25 11:40:53 -0400

    Create 287. Duplicate Number in an Array.py

---
+++ 287. Duplicate Number in an Array.py
@@ -0,0 +1,8 @@
+def findDuplicate(nums: List[int]) -> List[int]:
+    for num in nums:
+        i = abs(num) - 1
+      
+        if nums[i] < 0:
+            return abs(num)
+            
+        nums[i] = -nums[i]
