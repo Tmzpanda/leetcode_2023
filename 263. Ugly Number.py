ec6344bfb35408fedb03e7bb0d95e641b7392c6c
Tim Zhang <tmzpanda@gmail.com>
2023-09-30 19:37:35 -0700

    Create 263. Ugly Number.py

---
+++ 263. Ugly Number.py
@@ -0,0 +1,10 @@
+def isUgly(n: int) -> bool:
+    if n <= 0:
+        return False
+
+    factors = [2, 3, 5]
+    for factor in factors:
+        while n % factor == 0:
+            n //= factor
+
+    return n == 1
