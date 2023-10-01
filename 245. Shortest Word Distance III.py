f90ab294c4c7378bfcdf587f4bc2eec88bdc216f
Tim Zhang <tmzpanda@gmail.com>
2023-09-30 20:14:32 -0700

    Create 245. Shortest Word Distance III.py

---
+++ 245. Shortest Word Distance III.py
@@ -0,0 +1,18 @@
+def shortestWordDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
+    size = len(wordsDict)
+    p1, p2 = -1, -1
+    res = sys.maxsize
+    
+    for i in range(size):
+        if word1 == word2 and wordsDict[i] == word1:
+            p2 = p1
+            p1 = i
+        elif wordsDict[i] == word1:
+            p1 = i
+        elif wordsDict[i] == word2:
+            p2 = i
+
+        if p1 != -1 and p2 != -1:
+            res = min(res, abs(p1 - p2))
+
+    return res
