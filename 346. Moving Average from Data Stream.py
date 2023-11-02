af749465d0142c6a04092d6aaf79849332ebb9c6
Tim Zhang <tmzpanda@gmail.com>
2023-11-01 19:31:42 -0700

    Create 346. Moving Average from Data Stream.py

---
+++ 346. Moving Average from Data Stream.py
@@ -0,0 +1,18 @@
+class MovingAverage:
+    def __init__(self, size: int):
+        self.size = size
+        self.total = 0
+        self.length = 0
+        self.queue = deque()
+      
+    def next(self, val: int) -> float:
+        if self.length < self.size:
+            self.queue.append(val)
+            self.length += 1
+            self.total += val      
+        else:
+            self.queue.append(val)
+            self.total -= self.queue.popleft()
+            self.total += val
+            
+        return self.total/self.length
