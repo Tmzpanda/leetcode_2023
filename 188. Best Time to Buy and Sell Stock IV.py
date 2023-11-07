5690c01418d24378497249e318c381b09db08ac4
Tim Zhang <tmzpanda@gmail.com>
2023-11-07 11:58:02 -0800

    Update 188. Best Time to Buy and Sell Stock IV.py

--- 188. Best Time to Buy and Sell Stock IV.py
+++ 188. Best Time to Buy and Sell Stock IV.py
@@ -23,7 +23,21 @@ def maxProfit(k, prices):
 
     return dp[-1][-1]
 
+# 3D dp
+def maxProfit(self, k: int, prices: List[int]) -> int:
+    n = len(prices)
+    dp = [[[float('-inf')]*(k+1) for i in range(2)] for j in range(len(prices))]
 
+    for i in range(len(prices)):
+        dp[i][0][0] = 0
+    dp[0][1][0] = -prices[0]
+
+    for t in range(k+1):
+        for i in range(1,len(prices)):
+            dp[i][1][t] = max(dp[i-1][0][t]-prices[i], dp[i-1][1][t])
+            dp[i][0][t] = max(dp[i-1][0][t], dp[i-1][1][t-1] + prices[i])
+    
+    return max(dp[n-1][0])
 
 
 # dp state machine
