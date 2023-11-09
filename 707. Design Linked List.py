a7b665506576d03ae21dd5f68c64a0a9b47fe22b
Tim Zhang <tmzpanda@gmail.com>
2023-11-09 09:35:11 -0800

    Update 707. Design Linked List.py

--- 707. Design Linked List.py
+++ 707. Design Linked List.py
@@ -54,4 +54,79 @@ class MyLinkedList:
 
 
 # DoubleLinkedList
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
 
+class MyLinkedList:
+    def __init__(self):
+        self.head = ListNode(-1)
+        self.tail = ListNode(-1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.count = 0
+        
+    def get(self, index: int) -> int:
+        if index < 0 or index >= self.count:
+            return -1
+
+        curr = self.head
+        for _ in range(index + 1):
+            curr = curr.next
+
+        return curr.val
+        
+    def addAtHead(self, val: int) -> None:
+        pred, succ = self.head, self.head.next
+        node = ListNode(val)
+
+        node.prev = pred
+        node.next = succ
+        pred.next = node
+        succ.prev = node
+        self.count += 1
+        
+    def addAtTail(self, val: int) -> None:
+        pred, succ = self.tail.prev, self.tail
+        node = ListNode(val)
+
+        node.prev = pred
+        node.next = succ
+        pred.next = node
+        succ.prev = node
+        self.count += 1
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if index > self.count:
+            return
+        if index < 0:
+            index = 0
+
+        pred = self.head
+        for _ in range(index):
+            pred = pred.next
+        succ = pred.next
+
+        node = ListNode(val)
+        pred.next = node
+        succ.prev = node
+        node.prev = pred
+        node.next = succ
+
+        self.count += 1
+        
+    def deleteAtIndex(self, index: int) -> None:
+        if index < 0 or index >= self.count:
+            return
+
+        pred = self.head
+        for _ in range(index):
+            pred = pred.next
+        succ = pred.next.next
+
+        pred.next = succ
+        succ.prev = pred
+
+        self.count -= 1
