53bf4041bae0bef853e3169029351002f563fbc1
Tim Zhang <tmzpanda@gmail.com>
2023-11-15 17:30:41 -0800

    Update 4. Median of Two Sorted Arrays.py

--- 4. Median of Two Sorted Arrays.py
+++ 4. Median of Two Sorted Arrays.py
@@ -24,3 +24,31 @@ def findMedianSortedArrays(nums1: List[int], nums2: List[int]) -> float:
         return r
     else:
         return (l+r)/2
+
+
+# binary search
+def findMedianSortedArrays(nums1: List[int], nums2: List[int]) -> float:
+    if len(nums1) > len(nums2):
+        nums1, nums2 = nums2, nums1
+
+    m, n = len(nums1), len(nums2)
+    l, r = 0, m
+    while l <= r:
+        i = (l + r) // 2  # nums1
+        j = (m + n + 1) // 2 - i  # nums2
+
+        nums1L = nums1[i - 1] if i > 0 else float('-inf')
+        nums1R = nums1[i] if i < m else float('inf')
+        nums2L = nums2[j - 1] if j > 0 else float('-inf')
+        nums2R = nums2[j] if j < n else float('inf')
+
+        if nums1L <= nums2R and nums2L <= nums1R:
+            if (m + n) % 2 == 0:
+                return (max(nums1L, nums2L) + min(nums1R, nums2R)) / 2
+            else:
+                return max(nums1L, nums2L)
+        elif nums1L > nums2R:
+            r = i - 1
+        else:
+            l = i + 1
+
