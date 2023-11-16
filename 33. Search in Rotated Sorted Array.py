fd24bf95323e688ffdeb83cbfae23ad9432a0062
Tim Zhang <tmzpanda@gmail.com>
2023-11-16 14:42:30 -0800

    Create 33. Search in Rotated Sorted Array.py

---
+++ 33. Search in Rotated Sorted Array.py
@@ -0,0 +1,32 @@
+"""
+                   o 6
+                 o 5
+               o 4
+             ---------------           
+                          o 3
+                        o 2  
+                      o 1
+"""
+def search(nums: List[int], target: int) -> int:
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        mid = (l + r) // 2
+
+        if nums[mid] == target:
+            return mid
+
+        # if left side is sorted
+        if nums[l] <= nums[mid]:
+            if nums[l] <= target < nums[mid]:
+                r = mid - 1
+            else:
+                l = mid + 1
+        # if right side is sorted
+        else:
+            if nums[mid] < target <= nums[r]:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+    return -1
