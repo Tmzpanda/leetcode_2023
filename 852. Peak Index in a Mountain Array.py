839bcb523afd3774f3f52b4b0c7011505edfe2c2
Tim Zhang <tmzpanda@gmail.com>
2023-11-16 15:49:34 -0800

    Update 852. Peak Index in a Mountain Array.py

--- 852. Peak Index in a Mountain Array.py
+++ 852. Peak Index in a Mountain Array.py
@@ -1,11 +1,13 @@
 def peakIndexInMountainArray(arr: List[int]) -> int:
     l, r = 0, len(arr) - 1
 
-    while l < r:
+    while l <= r:
         mid = (l + r) // 2
         if arr[mid] > arr[mid + 1]:
-            r = mid
+            index = mid
+            r = mid - 1
         else:
             l = mid + 1
 
-    return l
+    return index
+            
