1b3e081c86fd034067518be0fde711e64aa2fc91
Tim Zhang <tmzpanda@gmail.com>
2023-11-16 14:14:53 -0800

    Update 215. Kth Largest Element in an Array.py

--- 215. Kth Largest Element in an Array.py
+++ 215. Kth Largest Element in an Array.py
@@ -1,3 +1,22 @@
+# quick select
+def findKthLargest(self, nums: List[int], k: int):      
+    k = len(nums) - k  # convert kth largest to kth smallest
+
+    def quickSelect(l, r):
+        pivot = nums[r]
+        p = l
+        for i in range(l, r):
+            if nums[i] <= pivot:
+                nums[p], nums[i] = nums[i], nums[p]
+                p += 1
+        nums[p], nums[r] = nums[r], nums[p]
+
+        if p > k: return quickSelect(l, p - 1)
+        elif p < k: return quickSelect(p + 1, r)
+        else: return nums[p]
+
+    return quickSelect(0, len(nums) - 1)
+
 # heap
 def findKthLargest(nums: List[int], k: int):      
     heap = []
@@ -8,5 +27,5 @@ def findKthLargest(nums: List[int], k: int):
           
     return heappop(heap)
 
-# quick select
+
 
