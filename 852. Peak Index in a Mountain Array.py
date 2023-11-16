29dd64c4c2a7885d97052aa8f7ac932f907ad3e6
Tim Zhang <tmzpanda@gmail.com>
2023-11-16 15:18:43 -0800

    Create 852. Peak Index in a Mountain Array.py

---
+++ 852. Peak Index in a Mountain Array.py
@@ -0,0 +1,11 @@
+def peakIndexInMountainArray(arr: List[int]) -> int:
+    l, r = 0, len(arr) - 1
+
+    while l < r:
+        mid = (l + r) // 2
+        if arr[mid] > arr[mid + 1]:
+            r = mid
+        else:
+            l = mid + 1
+
+    return l
