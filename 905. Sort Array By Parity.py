82c43e2c56790eeb34454d63e6130480b231c43a
Tim Zhang <tmzpanda@gmail.com>
2023-11-15 17:52:24 -0800

    Create 905. Sort Array By Parity.py

---
+++ 905. Sort Array By Parity.py
@@ -0,0 +1,14 @@
+def sortArrayByParity(nums: List[int]) -> List[int]:
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        if nums[l] % 2 == 1 and nums[r] % 2 == 0:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+        elif nums[l] % 2 == 0:
+            l += 1
+        elif  nums[r] % 2 == 1:
+            r -= 1
+            
+    return nums
