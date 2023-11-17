fa112a1a3ccd0f80620542868e0c87ad5d6e25f3
Tim Zhang <tmzpanda@gmail.com>
2023-11-16 17:43:31 -0800

    Update 704. Binary Search.py

--- 704. Binary Search.py
+++ 704. Binary Search.py
@@ -11,3 +11,20 @@ def search(nums: List[int], target: int) -> int:
             r = mid - 1
             
     return -1
+
+def search(nums: List[int], target: int) -> int:
+    def binarySearch(l, r):
+        if l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                return binarySearch(mid + 1, r)
+            else:
+                return binarySearch(l, mid - 1)
+        else:
+            return -1
+            
+    return binarySearch(0, len(nums) - 1)
+    
+
