e0a8fc0b0ec1a165fc6e76e5b95539c60269a97f
Tim Zhang <tmzpanda@gmail.com>
2023-11-17 10:47:03 -0800

    Update 2195. Append K Integers With Minimal Sum.py

--- 2195. Append K Integers With Minimal Sum.py
+++ 2195. Append K Integers With Minimal Sum.py
@@ -1,3 +1,29 @@
+# O(n)
+def minimalKSum(nums: List[int], k: int) -> int:
+    nums.sort() 
+    
+    i, target = 0, 1
+    res = 0
+    while i < len(nums):
+        if nums[i] == target:
+            i += 1
+            target += 1
+        elif nums[i] > target:
+            res += target
+            k -= 1
+            if k == 0:
+                return res
+            target += 1
+        else:
+            i += 1  # deduplicate
+
+    while k > 0:
+        res += target
+        target += 1
+        k -= 1
+
+    return res
+
 def minimalKSum(nums: List[int], k: int) -> int:
     nums = list(set(nums))
     nums.sort()
