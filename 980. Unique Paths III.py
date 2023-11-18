18b92444979e16dda5c0bcdebf2efa7a9b3e63f4
Tim Zhang <tmzpanda@gmail.com>
2023-11-18 13:50:28 -0800

    Update 980. Unique Paths III.py

--- 980. Unique Paths III.py
+++ 980. Unique Paths III.py
@@ -10,19 +10,18 @@ def uniquePathsIII(grid: List[List[int]]) -> int:
                 empty += 1
 
     res = 0
-    def dfs(i, j, path):
-        nonlocal res
-        if grid[i][j] == 2 and path == empty+1:
-            res += 1
+    def dfs(i, j, empty):
+        nonlocal res    # nonlocal
+        if grid[i][j] == 2:
+            if empty == -1:
+                res += 1
             return
         
         grid[i][j] = -1 
         for x, y in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
             if 0 <= x < m and 0 <= y < n and grid[x][y] != -1: 
-                dfs(x, y, path+1)
-        grid[i][j] = 0  # backtrack
+                dfs(x, y, empty-1)
+        grid[i][j] = 0      # backtrack
     
-    dfs(start_i, start_j, 1)
+    dfs(start_i, start_j, empty)
     return res
-
-# bit mask
