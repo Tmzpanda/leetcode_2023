871c8527b7a5af7da0ba47b162ba2f77d002c7bf
Tim Zhang <tmzpanda@gmail.com>
2023-11-18 13:37:29 -0800

    Create 980. Unique Paths III.py

---
+++ 980. Unique Paths III.py
@@ -0,0 +1,28 @@
+# backtrack
+def uniquePathsIII(grid: List[List[int]]) -> int:
+    m, n = len(grid), len(grid[0]) 
+    empty = 0 
+    for i in range(m):
+        for j in range(n): 
+            if grid[i][j] == 1: 
+                start_i, start_j = i, j
+            elif grid[i][j] == 0: 
+                empty += 1
+
+    res = 0
+    def dfs(i, j, path):
+        nonlocal res
+        if grid[i][j] == 2 and path == empty+1:
+            res += 1
+            return
+        
+        grid[i][j] = -1 
+        for x, y in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
+            if 0 <= x < m and 0 <= y < n and grid[x][y] != -1: 
+                dfs(x, y, path+1)
+        grid[i][j] = 0  # backtrack
+    
+    dfs(start_i, start_j, 1)
+    return res
+
+# bit mask
