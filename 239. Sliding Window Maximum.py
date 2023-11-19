3975022c237b7e5a6636713ac48e0e9b101dc380
Tim Zhang <tmzpanda@gmail.com>
2023-11-19 12:51:20 -0800

    Create 239. Sliding Window Maximum.py

---
+++ 239. Sliding Window Maximum.py
@@ -0,0 +1,32 @@
+# brute force O((n-k)*k)
+def maxSlidingWindow(nums: List[int], k: int) -> List[int]:
+    n = len(nums)
+    res = []
+    for i in range(n - k + 1):
+        res.append(max(nums[i:i+k]))
+
+    return res
+
+# mono-decreasing queue
+def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+    q = deque()
+    res = []
+
+    l, r = 0, 0
+    while r < len(nums):
+        # mono-increasing
+        while q and nums[q[-1]] < nums[r]:
+            q.pop()
+        q.append(r)
+
+        if l > q[0]:
+            q.popleft()
+
+        if r+1 >= k:
+            res.append(nums[q[0]])
+            l += 1
+            
+        r += 1
+
+    return res
+
