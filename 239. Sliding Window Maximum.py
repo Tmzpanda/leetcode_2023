598b518a81ef8743ee0a390efebf5f21ee824ad8
Tim Zhang <tmzpanda@gmail.com>
2023-11-19 14:10:31 -0800

    Update 239. Sliding Window Maximum.py

--- 239. Sliding Window Maximum.py
+++ 239. Sliding Window Maximum.py
@@ -14,7 +14,7 @@ def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
 
     l, r = 0, 0
     while r < len(nums):
-        while q and nums[q[-1]] < nums[r]: # mono-increasing
+        while q and nums[q[-1]] < nums[r]: # mono-decreasing
             q.pop()
         q.append(r)
 
