36846ac3ba21b02f1ec6c7f0604aa1177bf4e702
Tim Zhang <tmzpanda@gmail.com>
2023-11-18 16:25:46 -0800

    Update 7. Reverse Integer.py

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -2,3 +2,20 @@ def reverse(x: int) -> int:
     res = -int(str(x)[::-1].strip('-')) if x < 0 else int(str(x)[::-1])
     return res if res in range(-2**31, 2**31) else 0
 
+def reverse(self, x: int) -> int:
+    MIN = -2**31
+    MAX = 2**31 - 1
+
+    res = 0
+    while x:
+        # x, remainder = divmod(x, 10)
+        x, remainder = int(x/10), int(math.fmod(x, 10))
+        if (res > MAX//10) or (res == MAX//10 and remainder >= MAX%10):
+            return 0
+        if (res < MIN//10) or (res == MIN//10 and remainder <= MIN%10):
+            return 0
+
+        res = res*10 + remainder
+
+    return res
+
