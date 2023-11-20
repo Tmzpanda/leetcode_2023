c0d55478903c44636d31cee48b52aceeccd5e9df
Tim Zhang <tmzpanda@gmail.com>
2023-11-20 14:38:07 -0800

    Update 91. Decode Ways.py

--- 91. Decode Ways.py
+++ 91. Decode Ways.py
@@ -26,3 +26,22 @@ def numDecodings(s: str) -> int:
         return False
 
     return dfs(s)
+
+# dp
+def numDecodings(s: str) -> int:
+    if not s or s[0] == '0':
+        return 0
+
+    dp = [0] * (len(s) + 1)
+    dp[0] = 1 
+    dp[1] = 1 
+
+    for i in range(2, len(s) + 1):
+        # one digit
+        if s[i-1] != '0':
+            dp[i] += dp[i-1]
+        # two digits
+        if 10 <= int(s[i-2:i]) <= 26:
+            dp[i] += dp[i-2]
+
+    return dp[len(s)]
