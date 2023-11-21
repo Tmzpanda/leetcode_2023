bf0c106465895cfa5062bd0551343c0bdd0bfb54
Tim Zhang <tmzpanda@gmail.com>
2023-11-21 12:52:23 -0800

    Update 980. Unique Paths III.py

--- 980. Unique Paths III.py
+++ 980. Unique Paths III.py
@@ -25,3 +25,30 @@ def uniquePathsIII(grid: List[List[int]]) -> int:
     
     dfs(start_i, start_j, empty)
     return res
+
+def uniquePathsIII(self, grid: List[List[int]]) -> int:
+    m, n = len(grid), len(grid[0]) 
+    mask = 0    # represent the state of each cell
+    for i in range(m):
+        for j in range(n): 
+            if grid[i][j] == 1: 
+                start_i, start_j = i, j
+            if grid[i][j] == -1 or grid[i][j] == 1: 
+                mask ^= 1 << i*n+j  # set bit
+
+    @cache
+    def dfs(i, j, mask):
+        if grid[i][j] == 2 and mask == (1<<m*n)-1:  # if all bits are set
+            return 1
+        
+        res = 0
+        for x, y in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
+            if 0 <= x < m and 0 <= y < n:
+                k = x*n+y
+                if not mask & 1<<k: # if kth bit is set
+                    res += dfs(x, y, mask ^ 1<<k)
+                
+        return res
+    
+    
+    return dfs(start_i, start_j, mask)
