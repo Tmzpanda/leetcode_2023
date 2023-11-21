d87e02ab4d37bc217fe430d7a7b1bc5d1fcef44e
Tim Zhang <tmzpanda@gmail.com>
2023-11-21 14:26:52 -0800

    Update 309. Best Time to Buy and Sell Stock with Cooldown.py

--- 309. Best Time to Buy and Sell Stock with Cooldown.py
+++ 309. Best Time to Buy and Sell Stock with Cooldown.py
@@ -2,26 +2,26 @@
 """
 prices = [7,1,5,3,6,4]
       state    
-        0    1
- i  0   0    -7
-    1   o
-    2        o
-    3        x
-    4   o    o
+        0    1      2
+ i  0   0    -7     0
+    1   
+    2        
+    3        
+    4   o           o
     5   x denotes the maximum profit that can be obtained till day i, with state 0/1 (noShare/withShare)
 
 """
-def maxProfit(prices: List[int]) -> int:
+def maxProfit(self, prices: List[int]) -> int:
     n = len(prices)
-    dp = [[0]*2 for _ in range(n)]
+    dp = [[0]*3 for _ in range(n)]
     dp[0][1] = -prices[0]
-    
+        
     for i in range(1, n):
         dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
-        dp[i][1] = max(dp[i-1][1], (dp[i-2][0] if i > 1 else 0) - prices[i])
-    
+        dp[i][1] = max(dp[i-1][1], dp[i-1][2]-prices[i])
+        dp[i][2] = max(dp[i-1][2], dp[i-1][0])
+      
     return dp[n-1][0]
-
 
 # state machine
 def maxProfit(prices: List[int]) -> int:
