0f3885bbfaa082c8cf30132c97711008124f947e
Tim Zhang <tmzpanda@gmail.com>
2023-11-22 11:40:26 -0800

    Create 986. Interval List Intersections.py

---
+++ 986. Interval List Intersections.py
@@ -0,0 +1,16 @@
+def intervalIntersection(A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
+    res = []
+    i, j = 0, 0
+    while i < len(A) and j < len(B):
+
+        l = max(A[i][0], B[j][0])
+        r = min(A[i][1], B[j][1])
+        if l <= r:
+            res.append([l, r])
+
+        if A[i][1] < B[j][1]:
+            i += 1
+        else:
+            j += 1
+
+    return res
