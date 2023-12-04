fe07dde12472755cf0ef5a59a608736daa034526
Tim Zhang <tmzpanda@gmail.com>
2023-12-04 10:37:06 -0800

    Update 104. Maximum Depth of Binary Tree.py

--- 104. Maximum Depth of Binary Tree.py
+++ 104. Maximum Depth of Binary Tree.py
@@ -8,3 +8,18 @@ def maxDepth(root: Optional[TreeNode]) -> int:
     return max(left_height, right_height) + 1 
 
 
+def maxDepth(root: Optional[TreeNode]) -> int:
+    max_depth = 0
+
+    def dfs(node, depth):
+        nonlocal max_depth
+        if not node:
+            return
+
+        depth += 1
+        max_depth = max(max_depth, depth)
+        dfs(node.left, depth)
+        dfs(node.right, depth)
+
+    dfs(root, 0)
+    return max_depth
