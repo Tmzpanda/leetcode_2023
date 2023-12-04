d6987788c2013e6c1f48807d23593fdb70ab9be1
Tim Zhang <tmzpanda@gmail.com>
2023-12-04 10:06:24 -0800

    Update 112. Path Sum.py

--- 112. Path Sum.py
+++ 112. Path Sum.py
@@ -10,16 +10,18 @@ def hasPathSum(root: Optional[TreeNode], targetSum: int) -> bool:
 def hasPathSum(root: Optional[TreeNode], targetSum: int) -> bool:
     has_path = False
     def dfs(node, current_sum):
+        nonlocal has_path
         if not node:
             return
         current_sum += node.val
-        if not node.left and not node.right and current_sum == sum:
+        if not node.left and not node.right and current_sum == targetSum:
             has_path = True
             return
         
         dfs(node.left, current_sum)
         dfs(node.right, current_sum)
 
+    dfs(root, 0)
     return has_path
-    
+
 
