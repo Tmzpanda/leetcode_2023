c786a811fede9a9c97363bc330472f5df54a22c1
Tim Zhang <tmzpanda@gmail.com>
2023-12-03 21:35:32 -0800

    Update 276. Paint Fence.py

--- 276. Paint Fence.py
+++ 276. Paint Fence.py
@@ -5,16 +5,12 @@ num_ways(i) = num_ways_diff(i) + num_ways_same(i)
             = (num_ways(i-1)+num_ways(i-2)) * (k-1)
             
 """
+
 def numWays(n: int, k: int) -> int:
-    if n == 0: return 0
-    if n == 1: return k
-    if n == 2: return k*k
+    dp = [0] * n
+    dp[0], dp[1] = k, k*k
 
-    dp = [0] * (n + 1)
-    dp[1] = k
-    dp[2] = k * k
-    for i in range(3, n + 1):
-        dp[i] = (dp[i - 1] + dp[i - 2]) * (k - 1)
+    for i in range(2, n):
+        dp[i] = (k-1) * (dp[i-1]+dp[i-2])
 
-    return dp[n]
-
+    return dp[n-1]
