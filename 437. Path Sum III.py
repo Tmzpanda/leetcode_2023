602ae5ce8d93d92fb3236fc72b8148c1d47df8a7
Tim Zhang <tmzpanda@gmail.com>
2023-12-04 16:01:43 -0800

    Create 437. Path Sum III.py

---
+++ 437. Path Sum III.py
@@ -0,0 +1,22 @@
+def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+    psum_dict = defaultdict(int)
+    psum_dict[0] = 1
+    count = 0
+
+    def dfs(node, psum):
+        nonlocal count
+        if not node:
+            return
+        
+        psum += node.val
+        if psum-targetSum in psum_dict:
+            count += psum_dict[psum-targetSum]
+        psum_dict[psum] += 1
+
+        dfs(node.left, psum)
+        dfs(node.right, psum)
+
+        psum_dict[psum] -= 1
+
+    dfs(root, 0)
+    return count
