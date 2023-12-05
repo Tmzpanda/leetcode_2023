1c3436eda84d574df2a6cfa46e0226eef165726b
Tim Zhang <tmzpanda@gmail.com>
2023-12-05 10:46:08 -0800

    Create 124. Binary Tree Maximum Path Sum.py

---
+++ 124. Binary Tree Maximum Path Sum.py
@@ -0,0 +1,16 @@
+def maxPathSum(root: Optional[TreeNode]) -> int:
+    res = float('-inf')
+
+    def dfs(root):
+        nonlocal res
+        if not root:
+            return 0
+        
+        leftMax = dfs(root.left)
+        rightMax = dfs(root.right)
+        res = max(res, root.val + leftMax + rightMax)
+
+        return max(root.val + max(leftMax, rightMax), 0)
+
+    dfs(root)
+    return res
