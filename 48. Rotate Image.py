c909a3216dc19a8f4e27f857aac51e7b661a95fc
Tim Zhang <tmzpanda@gmail.com>
2023-12-06 12:29:01 -0800

    Update 48. Rotate Image.py

--- 48. Rotate Image.py
+++ 48. Rotate Image.py
@@ -1,4 +1,4 @@
-# in-place
+# rotate groups of four
 def rotate(matrix: List[List[int]]) -> None:
     n = len(matrix)
     for i in range(n//2 + n%2):
@@ -9,13 +9,12 @@ def rotate(matrix: List[List[int]]) -> None:
             matrix[j][n-1-i] = matrix[i][j]
             matrix[i][j] = temp
 
-
-# general
-def rotate(matrix: List[List[int]]):
+# transpose and reverse
+def rotate(self, matrix: List[List[int]]) -> None:
     n = len(matrix)
-    res = [[0]*n for _ in range(n)]
     for i in range(n):
-        for j in range(n):
-            res[j][n-1-i] = matrix[i][j]
-
-    return res
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            
+    for i in range(n):
+        matrix[i].reverse()
