7d66900b7d290acd96a3ee8303f33449dbd77ff5
Tim Zhang <tmzpanda@gmail.com>
2023-12-07 10:24:07 -0800

    Update 1216. Valid Palindrome III.py

--- 1216. Valid Palindrome III.py
+++ 1216. Valid Palindrome III.py
@@ -1,18 +1,20 @@
 def isValidPalindrome(s: str, k: int) -> bool:
-    def longestPalindromeSubseq(s: str) -> int:
-        return longestCommonSubsequence(s, s[::-1])
-
-    def longestCommonSubsequence(text1: str, text2: str) -> int:
-        n, m = len(text1), len(text2)
-        dp = [[0]*(m+1) for _ in range(n+1)]
-
-        for i in range(1, n+1):
-            for j in range(1, m+1):
-                if text1[i-1] == text2[j-1]:
-                    dp[i][j] = dp[i-1][j-1] + 1
-                else:
-                    dp[i][j] = max(dp[i][j-1], dp[i-1][j])
-                    
-        return dp[n][m]
-
     return longestPalindromeSubseq(s) + k >= len(s)
+
+# dfs
+def isValidPalindrome(self, s: str, k: int) -> bool:
+    memo = {}
+    def dfs(i, j, k):
+        if k < 0: return False
+        if i >= j: return True
+        if (i, j, k) in memo:
+            return memo[(i, j, k)]
+
+        if s[i] == s[j]:
+            memo[(i, j, k)] = dfs(i+1, j-1, k) 
+        else:
+            memo[(i, j, k)] = dfs(i+1, j, k-1) or dfs(i, j-1, k-1)
+        return memo[(i, j, k)]
+
+    n = len(s)
+    return dfs(0, n-1, k)
