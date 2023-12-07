3d255fecbd33277f401b895c56cb94d4ec563962
Tim Zhang <tmzpanda@gmail.com>
2023-12-07 12:59:28 -0800

    Update 516. Longest Palindromic Subsequence.py

--- 516. Longest Palindromic Subsequence.py
+++ 516. Longest Palindromic Subsequence.py
@@ -18,3 +18,33 @@ def longestPalindromeSubseq(s: str) -> int:
     
     n = len(s)
     return dfs(0, n-1)
+
+# dp
+
+"""
+s = "bbbab"
+      j 
+      b b b a b
+  i b 1     o x
+    b   1   o o
+    b     1 
+    a       1
+    b         1  
+"""
+def longestPalindromeSubseq(s: str) -> int:
+    n = len(s)
+    dp = [[0]*n for _ in range(n)]
+    for i in range(n):
+        dp[i][i] = 1
+
+    for L in range(2, n+1):
+        for i in range(n-L+1):
+            j = i + L - 1
+            if s[i] == s[j] and L == 2:
+                dp[i][j] = 2
+            elif s[i] == s[j]:
+                dp[i][j] = dp[i+1][j-1] + 2
+            else:
+                dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+
+    return dp[0][n-1]
