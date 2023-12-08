22701869524efc79a6f430cbdeedab3fbb5f17b4
Tim Zhang <tmzpanda@gmail.com>
2023-12-07 17:18:33 -0800

    Update 698. Partition to K Equal Sum Subsets.py

--- 698. Partition to K Equal Sum Subsets.py
+++ 698. Partition to K Equal Sum Subsets.py
@@ -26,4 +26,29 @@ def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
 
     return dfs(0, k, 0)
 
-# 
+# bit mask, dp
+def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+    if sum(nums) % k:
+        return False
+    
+    n = len(nums)
+    target = sum(nums) // k
+    mask = 0
+
+    @cache
+    def dfs(i, k, subsetSum, mask):
+        if k == 0:
+            return True
+        if subsetSum == target:
+            return dfs(0, k-1, 0, mask)
+
+        for j in range(i, n):
+            if mask & 1<<j or subsetSum+nums[j] > target:
+                continue
+            
+            if dfs(j+1, k, subsetSum+nums[j], mask | 1<<j):
+                return True
+        
+        return False
+
+    return dfs(0, k, 0, mask)
