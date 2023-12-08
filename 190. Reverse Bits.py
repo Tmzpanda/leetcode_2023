5d06cb3b9d3bec044e2f397ca6b2417166255c19
Tim Zhang <tmzpanda@gmail.com>
2023-12-07 17:51:30 -0800

    Update 190. Reverse Bits.py

--- 190. Reverse Bits.py
+++ 190. Reverse Bits.py
@@ -1,7 +1,7 @@
-def reverseBits(n: int) -> int:
+def reverseBits(n: int) -> int:    # 00000000 00000000 00000000 00000101 
     res = 0
     for i in range(32):
         bit = (n >> i) & 1
-        res = res | (bit << (31 - i))
+        res = res | (bit << (31 - i))    # 10100000 00000000 00000000 00000000
 
     return res
