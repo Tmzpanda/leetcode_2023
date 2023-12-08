1c2ec9f00c5ad67df6f7055697a102a1f063bcba
Tim Zhang <tmzpanda@gmail.com>
2023-12-07 18:47:50 -0800

    Update 1071. Greatest Common Divisor of Strings.py

--- 1071. Greatest Common Divisor of Strings.py
+++ 1071. Greatest Common Divisor of Strings.py
@@ -1,25 +1,15 @@
 # O(min(m,n)⋅(m+n)) O(min(m,n))
 def gcdOfStrings(str1: str, str2: str) -> str:
-  
-    def isDivisor(l):
-        if len1%l or len2%l:
+    def isDivisor(a, b, x):
+        if a % x or b % x: 
             return False
-        f1, f2 = len1//l, len2//l
-        return str1[:l]*f1 == str1 and str1[:l]*f2 == str2
 
-    len1, len2 = len(str1), len(str2)
-    for l in range(min(len1, len2), 0, -1):
-        if isDivisor(l):
-            return str1[:l]
+        f1, f2 = a // x, b // x
+        return str1[:x]*f1 == str1 and str1[:x]*f2 == str2
+
+    a, b = len(str1), len(str2)
+    for x in range(min(a, b), 0, -1): # greedy
+        if isDivisor(a, b, x):
+            return str1[:x]
 
     return ""
-
-
-def gcdOfStrings(str1: str, str2: str) -> str:
-    # check if they have non-zero GCD string.
-    if str1 + str2 != str2 + str1:
-        return ""
-
-    # get the GCD of the two lengths.
-    max_length = gcd(len(str1), len(str2))
-    return str1[:max_length]
